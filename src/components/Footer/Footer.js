d3acde587fd993880fdebe861064e62dad8c1ef8
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2022-10-22 17:32:50 +0000

    home add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer>
-      ©Svartrotta ehf  - <a href="https://github.com/ArniZenux/translate">Sjá github</a>
+      ©Svartrotta ehf  - <a href="https://github.com/ArniZenux/reactTranslate">Sjá github</a>
     </footer>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
 export function Home() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(null);
+  const [text, setText] = useState('');
+  //const [error, setError] = useState(null);
+  //const [loading, setLoading] = useState(null);
+  
+  let teljari = 0 ; 
+  const max = 1000; 
   
   const recounter = e => {
-    setCount(e.target.value.length);
+    teljari = e.target.value.length; 
+    if( teljari <= max ) setCount(teljari);
   }
 
-  function handleSubmit(e){
-    e.preventDefault();
-    console.log("balblbalab");
+  let success = true; 
+
+  const onSubmit = async(e) => {
+    console.log(text);
+    const data =  { text };
+    console.log(data); 
+    
+    const requestOptions = {
+      //method: 'POST',
+      //headers: {"Content-Type": "application/json" },
+      //body: JSON.stringify(data)
+    };
+
+    success = await fetch(apiUrl + '/tulkur/adduser', requestOptions);
+    
+    /*if(success){
+      history.push('/tulkur');
+    }
+    else{
+      console.log("Virkar ekki");
+    }*/
   }
-  
+    
   return(
     <div class="grid">
       <div class="row">
@@ -19,18 +47,20 @@ export function Home() {
           <div class="categories">
             <section>
               <h2>English</h2>
-                <form action="/translate" method="POST">
+                <form onSubmit={onSubmit}>
                   <div class="smallBox">
                     <textarea 
                       class="textarea" 
                       id="xtext" 
-                      name="text" 
+                      name="text"
+                      value={text}
+                      maxLength={1000} 
                       onChange={ recounter } 
                     />
                       <div class="small_button_box">
                         <button type="submit" class="button">Translate</button>
                           <div id="counter">
-                            <span> <p> { count } </p> </span>
+                            <span> <p> { count } / 1000 </p> </span>
                           </div>
                       </div>
                   </div>
@@ -43,12 +73,9 @@ export function Home() {
         <div class="categories">
             <section>
               <h2>Icelandic</h2>
-                <form onSubmit={handleSubmit}>
+                <form>
                   <div class="smallBox">
                     <textarea class="textarea" id="newText" name="newText"></textarea>
-                      <div class="small_button_box">
-                        <button type="submit" class="button">Translate</button>
-                      </div>
                   </div>
               </form>
             </section>

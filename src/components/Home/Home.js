8b8f568c29e80bdc919a0a016bb3734c7832e6da
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2022-10-23 17:50:23 +0000

    useEffect add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,64 +1,91 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export function Home() {
   const [count, setCount] = useState(null);
-  const [text, setText] = useState('');
-  //const [error, setError] = useState(null);
-  //const [loading, setLoading] = useState(null);
+  //const [text, setText] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
   
-  let teljari = 0 ; 
+  let teljari = 0; 
   const max = 1000; 
   
   const recounter = e => {
     teljari = e.target.value.length; 
     if( teljari <= max ) setCount(teljari);
   }
-
-  let success = true; 
-
-  const onSubmit = async(e) => {
-    console.log(text);
-    const data =  { text };
-    console.log(data); 
+  
+  useEffect(() => {
+    async function getData(){
+    setLoading(true);
+    setError(null); 
+    let json;
     
+    try {
+      const result = await fetch(apiUrl + `/translate`); 
+      console.log(result); 
+      
+      if(!result.ok){
+        throw new Error('Ekki ok');
+      }
+      json = await result.json();
+    }
+    catch(e){
+      console.warn('unable to fetch data', e); 
+      setError('Gat ekki sótt efni í vefþjónustu - Bilað í þjónustuna.');
+      return; 
+    }
+    finally{
+      setLoading(false); 
+    }
+    }
+ 
+  }, []);
+
+  const onSubmit = async (e) => {
+    console.log(apiUrl); 
+  }
+  /*const onSubmit = async (e) => {
+    let success = true; 
+    const info = 'arni';
+    console.log('Hello log');
     const requestOptions = {
-      //method: 'POST',
-      //headers: {"Content-Type": "application/json" },
-      //body: JSON.stringify(data)
+      method: 'POST',
+      headers: {"Content-Type": "application/json" },
+      body: JSON.stringify(info)
     };
-
-    success = await fetch(apiUrl + '/tulkur/adduser', requestOptions);
     
-    /*if(success){
-      history.push('/tulkur');
+    console.log(apiUrl); 
+
+    success = await fetch(apiUrl + '/translate', requestOptions);
+    if(success){
+      console.log('success');
     }
     else{
-      console.log("Virkar ekki");
-    }*/
-  }
+      console.log(apiUrl);
+    }
+  }*/
     
   return(
-    <div class="grid">
-      <div class="row">
+    <div classNAme='grid'>
+      <div className='row'>
 
-        <div class="col col-6">
-          <div class="categories">
+        <div classNAme="col col-6">
+          <div className="categories">
             <section>
               <h2>English</h2>
                 <form onSubmit={onSubmit}>
-                  <div class="smallBox">
+                  <div className="smallBox">
                     <textarea 
-                      class="textarea" 
+                      className='textarea' 
                       id="xtext" 
                       name="text"
-                      value={text}
                       maxLength={1000} 
                       onChange={ recounter } 
                     />
-                      <div class="small_button_box">
-                        <button type="submit" class="button">Translate</button>
+                      <div className="small_button_box">
+                        <button type="submit" className="button">Translate</button>
                           <div id="counter">
                             <span> <p> { count } / 1000 </p> </span>
                           </div>
@@ -68,14 +95,14 @@ export function Home() {
             </section>
           </div>
         </div>
-        <div class="col col-6">
+        <div className="col col-6">
 
-        <div class="categories">
+        <div className="categories">
             <section>
               <h2>Icelandic</h2>
                 <form>
-                  <div class="smallBox">
-                    <textarea class="textarea" id="newText" name="newText"></textarea>
+                  <div className="smallBox">
+                    <textarea className='textarea' id="newText" name="newText"></textarea>
                   </div>
               </form>
             </section>
